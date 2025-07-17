d61cbd50af5f0ac5c1dd7dc9b5bb69dcdcebff53
aaronrader <aaronrader@protonmail.com>
2025-07-17 09:32:05 -0400

    Init

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadURL('http://localhost:3000'); // URL of the React app
+    win.maximize();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
