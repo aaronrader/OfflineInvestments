972bef3b1a55a1ffb8e7513468a70a00effff155
aaronrader <aaronrader@protonmail.com>
2025-08-25 12:30:32 -0400

    Installed Electron-Forge

--- main.js
+++ main.js
@@ -15,22 +15,25 @@ function createWindow() {
             contextIsolation: true,
             preload: path.join(dirname(fileURLToPath(import.meta.url)), "preload.js")
         },
+        autoHideMenuBar: true,
     });
 
     //win.loadURL('http://localhost:3000'); // URL of the React app
     win.loadFile(path.join(dirname(fileURLToPath(import.meta.url)), 'build', 'index.html'));
 }
 
-app.whenReady().then(createWindow);
+app.setAppUserModelId("com.squirrel.AppName.AppName");
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+app.whenReady().then(createWindow);
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
+    }
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
 });
